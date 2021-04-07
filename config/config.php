84bc5701f5aff63839d164f6f1225150715a49b8
Bezdelnik12 <75016725+Bezdelnik12@users.noreply.github.com>
2021-04-07 18:15:50 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,6 @@
 /**
  * Настройки системы
  *
- * @author Sergej Rufov <rufov@freeun.ru>
  * @author Bezdelnik12 <nik.shmelev.24@gmail.com>
  * @license https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
